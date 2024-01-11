be583c364010ca8391f56e4d605e07b372b4ff36
vuongc4298 <156062555+vuongc4298@users.noreply.github.com>
2024-01-11 10:34:07 +0000

    Q3 update
    3 functions of addition

--- Lab02/a/Q3.py
+++ Lab02/a/Q3.py
@@ -1,15 +1,25 @@
-def addition(n):
+def addition_recursive(n):
     if n == 0:
         return 0
-    return n + addition(n-1)
+    return n + addition_recursive(n-1)
+
+def addition_formula(n):
+    return n*(n+1)/2
+
+def addition_loop(n):
+    result = 0
+    for i in range(1, n+1):
+        result += i
+    return result
 
 def display_student(name, age):
     print(name, age)
 
 show_student = display_student
 
-res = addition(10)
-print(res)
+print(addition_formula(10))
+print(addition_loop(10))
+print(addition_recursive(10))
 
 display_student("Emma", 26)
 show_student("Emma", 26)
