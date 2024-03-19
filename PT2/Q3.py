396a36581ad5780504bff357d77bf50c153ab251
vuongc4298 <156062555+vuongc4298@users.noreply.github.com>
2024-03-19 08:25:20 +0000

    Add files via upload

--- PT2/Q3.py
+++ PT2/Q3.py
@@ -1,33 +1,31 @@
 def knapsack_greedy(values, weights, max_weight):
-    n = len(values)
-    items = sorted(zip(values, weights), key=lambda x: x[0]/x[1], reverse=True)
+    cnt = dict()
+    items = sorted(zip(values, weights), key=lambda x: (x[0]/x[1], x[0]), reverse=True)
+    # print(items)
     total_value = 0
     for v, w in items:
-        if max_weight >= w:
+        while max_weight >= w:
             max_weight -= w
             total_value += v
-        else:
-            total_value += max_weight * (v/w)
-            break
-    return total_value
+            if (v, w) not in cnt:
+                cnt[(v, w)] = 1
+            else:
+                cnt[(v, w)] += 1
+    return total_value, cnt
 
 fin = "BAD.INP"
-dic = dict()
+values = list()
+weights = list()
 with open(fin, 'r') as file:
     first = list(map(int, file.readline().strip().split(' ')))
     n = int(first[0])
     w = int(first[1])
     for line in file.readlines():
         lst = line.strip().split(' ')
-        dic[lst[2]] = (lst[0], lst[1])
-values = list()
-weights = list()
-for entry in dic:
-    values.append(int(dic[entry][1]))
-    weights.append(int(dic[entry][0]))
-max_values = knapsack_greedy(values, weights, w)
-print(n)
-print(w)
+        values.append(int(lst[0]))
+        weights.append(int(lst[1]))
+
+max_values, cnt = knapsack_greedy(values, weights, w)
 print(max_values)
-# print(values)
-# print(weights)
+for item in cnt:
+    print(f"{item}: {cnt[item]}")
